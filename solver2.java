ab01621edda60b1909246969eec89b52c7aa80dd
tristygo <tphillips2104@gmail.com>
2025-09-16 17:18:25 +0200

    started v2 of solver

---
+++ solver2.java
@@ -0,0 +1,213 @@
+import java.util.BitSet;
+import java.util.Random;
+
+public class solver2 {
+
+    // Max runtime
+    private final static int T = 10000000;
+
+    static int numVars;
+    static int numClauses;
+    static int[] clauseCosts;
+    static int[] currentCosts;
+    static int[] literals;
+    static int[] values;
+    static BitSet vars;
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        
+        // Read in wcard file 
+        CapstoneFileReader reader = new CapstoneFileReader();
+        reader.readFile("test3.wcard");
+
+        // Get data from reader
+        numVars = reader.getNumVars();
+        numClauses = reader.getNumClauses();
+        clauseCosts = reader.getCosts();
+        currentCosts = new int[numClauses]; // initialise current cost list
+        literals = reader.getLiterals();
+        values = reader.getValues();
+
+        // Set up float for each clause
+        int[] clauseFloats = new int[numClauses];
+
+        // Boolean variables
+        vars = new BitSet(numVars);
+
+        // Set up boolean assignment
+        for (int i=0; i < numVars; i++)
+        {
+            // Initiate boolean assignment randomly
+            if (random.nextInt(2) == 1)
+            {
+                vars.set(i);
+            }
+        }
+
+        // Set up make and break scores for each variable
+        int[] makeScores = new int[numVars];
+        int[] breakScores = new int[numVars];
+
+        // Define flattened 2d array to link variables to clauses
+        int[] clauses = new int[numVars*numClauses];
+
+        // Link variables to clauses
+        for (int i=0; i < numClauses; i++)
+        {
+            for (int j=0; j < numVars; j++)
+            {
+                clauses[j*numClauses + i] = (literals[numVars*i+j] == 0) ? 0 : 1;
+            }   
+        }
+
+        // Calculate floats of initial state
+        int curTotalCost = 0;
+        for (int i=0; i < numClauses; i++)
+        {
+            clauseFloats[i] = checkFloat(i);
+        }
+
+        // Calculate make and break scores for each variable
+        for (int i=0; i < numVars; i++) // iterate through each variable
+        {
+            for (int j=0; j < numClauses; j++) // check each clause per variable
+            {
+                if (clauses[i*numClauses+j] == 1) // only consider clauses that contain the current variable
+                {
+                    if (clauseFloats[j] == 0) {} // increase break count
+                    else if (clauseFloats[j] == -1) {} // increase make count
+                }
+            }
+        }
+
+        System.out.println("Initial cost: " + (Integer.toString(curTotalCost))); ///
+
+        // Record overall best assignment and best score
+        BitSet bestAssignment = new BitSet(numVars);
+        int bestScore = Integer.MAX_VALUE;
+
+        // Create new int array to store updated costs for each clause
+        int[] updatedCosts = new int[numClauses];
+        int t = 0;
+        int minCost;
+        int minCostFlip;
+        int tempCost = 0;
+        while (true)
+        {
+            // for each variable:
+            //      flip, then update cost for affected clauses
+            //      sum to get "total cost of flip"
+            
+            minCost = curTotalCost; // for each pass, reset the min cost, and corresponding variable flip
+            minCostFlip = -1;
+            for (int i=0; i<numVars; i++)
+            {
+                // flip var
+                vars.flip(i);
+                tempCost = 0;
+
+                // recalculate cost of hypothetical scenario
+                for (int j=0; j < numClauses; j++)
+                {
+                    // If the current clause must be checked (i.e. has been affected by a flip), then update cost
+                    updatedCosts[j] = (clauses[i*numClauses+j] == 1) ? calcCurrentClauseCost(j) : currentCosts[j];
+                    tempCost = tempCost + updatedCosts[j];
+                }
+
+                vars.flip(i); // flip var back
+
+                // take min to get which flip results in lowest cost
+                if (tempCost < minCost) 
+                {
+                    minCost = tempCost;
+                    minCostFlip = i;
+                }
+                t++;
+            }       
+            
+            // Check if no improvements made, then restart with random assignments
+            if (minCostFlip == -1) 
+            {
+                if (minCost < bestScore)
+                {
+                    bestScore = minCost;
+                    bestAssignment = (BitSet)vars.clone();
+                    System.out.println("New Best Cost: " + Integer.toString(bestScore));
+                }
+
+                if (t > T) {break;} // if time is up, end run
+
+                // Assuming time isn't up - restart algorithm with randomly assigned vars
+                for (int i=0; i < numVars; i++)
+                {
+                    // Initiate boolean assignment randomly
+                    if (random.nextInt(2) == 1)
+                    {
+                        vars.set(i);
+                    }
+                }
+
+                minCostFlip = random.nextInt(numVars);
+            }
+
+            // flip variable and update clauses if it results in an improvement
+            vars.flip(minCostFlip);
+
+            // recalculate cost of current scenario
+            curTotalCost = 0;
+            for (int j=0; j < numClauses; j++)
+            {
+                // If the current clause must be checked (i.e. has been affected by a flip), then update cost
+                currentCosts[j] = (clauses[minCostFlip*numClauses+j] == 1) ? calcCurrentClauseCost(j) : currentCosts[j];
+                curTotalCost = curTotalCost + currentCosts[j];
+            }
+
+            //if (t > T) {break;} // if time is up, end run
+        }
+
+        System.out.println("Final Best Cost: " + Integer.toString(bestScore));
+
+        // Create string output
+        //String output = "(";
+        //for (int k=0; k < numVars-1; k++)
+        //{
+        //    output = output + ((bestAssignment.get(k)) ? "1" : "0") + ", ";
+        //}
+        //output = output + ((bestAssignment.get(numVars-1)) ? "1" : "0") + ")";
+
+        //System.out.println("Corresponding Assignment: " + output);
+    }
+
+    public static int checkFloat(int clauseToCheck)
+    {
+        int sum = 0;
+        // Loop over each variable that could occur in clause (all vars)
+        for (int i=0; i < numVars; i++)
+        {
+            // If a positive literal is mentioned, check if it is set, and if so, add to total value on LHS of expression
+            if (literals[numVars*clauseToCheck+i] > 0)
+            {
+                sum = sum + ((vars.get(i)) ? 1 : 0);
+            }
+            // If a negative literal is mentioned, check if it is NOT set, and if not, add to total value on LHS of expression
+            else if (literals[numVars*clauseToCheck+i] < 0)
+            {
+                sum = sum + ((vars.get(i)) ? 0 : 1);
+            }
+            // If the literal is 0, don't consider it (no "else" needed)
+        }
+        return (sum - values[clauseToCheck]); //return the float, i.e. sum - cost of clause (as we require sum to be >= cost for clause to be SAT)
+    }
+
+    public static boolean checkSAT(int clauseToCheck)
+    {
+        return (checkFloat(clauseToCheck) >= 0); //return whether the float is >= 0
+    }    
+
+    public static int calcCurrentClauseCost(int clause)
+    {   
+        // If clause is satisfied, cost is 0, otherwise return associated cost
+        return (checkSAT(clause)) ? 0 : clauseCosts[clause];
+    }
+}
