77073b7c313c077abb66e34600b5b5b4e219887b
Spitfyre347 <mikael.christodoulides@gmail.com>
2025-09-16 15:55:42 +0200

    Working on file reader/input parsing. Added renamed 'facade' void.

--- solver.java
+++ solver.java
@@ -19,7 +19,7 @@ public class solver {
         
         // Read in wcard file 
         CapstoneFileReader reader = new CapstoneFileReader();
-        reader.readFile("test3.wcard");
+        reader.InitializeClauses("test3.wcard", false);
 
         // Get data from reader
         numVars = reader.getNumVars();
