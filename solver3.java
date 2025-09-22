da54a8cb34e976f813c923d69bea576f0458e02d
tristygo <tphillips2104@gmail.com>
2025-09-22 23:05:12 +0200

    solver3 draft 1 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! lesgo

--- solver3.java
+++ solver3.java
@@ -1,5 +1,6 @@
 import java.util.BitSet;
 import java.util.Random;
+import java.util.ArrayList;
 
 public class solver3 {
 
@@ -20,7 +21,8 @@ public class solver3 {
     static int[] softCosts; // soft clause costs
     static int[] softFloats; // floats for each soft clause
     static int[] hardFloats; // floats for each hard clause
-    static int[] unsat; // unSAT soft clauses
+    static ArrayList<Integer> unsat; // unSAT soft clauses
+    static int[] unsat_arr; // unSAT soft clauses as primitive array
     static int[] dynamicCosts; // weighted soft clause costs
     static int[] softValues; // k-values for soft clauses
     static int[] hardValues; // k-values for hard clauses
@@ -48,6 +50,8 @@ public class solver3 {
         dynamicCosts = softCosts.clone(); // array for dynamic soft costs
         numSoft = softValues.length;
         numHard = hardValues.length;
+        softFloats = new int[softValues.length];
+        hardFloats = new int[hardValues.length];
 
         // Create Boolean variables
         vars = new BitSet(numVars);
@@ -65,23 +69,28 @@ public class solver3 {
 
         Random random = new Random();
 
-        // Calculate total cost of initial state and check which clauses are SAT
+        // Calculate float of hard clauses
+        for (int c=0; c < numHard; c++)
+        {
+            hardFloats[c] = checkFloat(c, true);
+        }
+
+        // Calculate:
+        // 1. total cost of initial state
+        // 2. float for each clause
+        // 3. SAT for each clause
         long curTotalCost = 0;
-        unsat = new int[numSoft];
-        int index = 0;
-        boolean sat = true;
+        unsat = new ArrayList<>();
+        boolean sat;
         for (int c=0; c < numSoft; c++)
         {
-            // if current clause is unSAT, add to array
-            sat = checkSAT(c);
-            if (!sat)
+            softFloats[c] = checkFloat(c, false);
+            sat = checkSATFloat(c, false); // if current clause is unSAT, add to array
+            if (!sat) 
             {
-                unsat[index] = c; 
-                index++;
-            }
-
-            // Add to total cost if not SAT
-            curTotalCost += (sat) ? 0 : softCosts[c]; 
+                unsat.add(c); // add to unsat arraylist
+                curTotalCost += softCosts[c]; // Add to total cost if not SAT
+            } 
         }
 
         System.out.println("Initial cost: "+String.valueOf(curTotalCost));
@@ -99,10 +108,16 @@ public class solver3 {
         final double RANDOM_CHANCE = 0.01;
         int curClause = -1;
         int start, end;
+        boolean skip = false;
+        // For score calculation
+        long bestScore = 0;
+        int bestFlip = -1;
+        int sign = 1;
         while (true)
         {
             // Algorithm:
-            // calculate cost and update best assignment
+            // update floats - ONLY AFFECTED CLAUSES
+            // calculate cost and update best assignment - ONLY AFFECTED CLAUSES
             // pick unsat soft clause, with prob weighted by dynamic cost
             // calculate break and make costs for variables in clause
             // outlaw any flips that violate hard clauses
@@ -110,23 +125,31 @@ public class solver3 {
             // a) variable with highest score: make - break
             // b) with small chance, random flip
 
-            // Calculate total cost of state and update unsat array
+            // Update floats - ONLY AFFECTED CLAUSES
+            // Calculate float of hard clauses
+            for (int c=0; c < numHard; c++)
+            {
+                hardFloats[c] = checkFloat(c, true);
+            }
+            // Calculate float of soft clauses
+            for (int c=0; c < numSoft; c++)
+            {
+                softFloats[c] = checkFloat(c, false);
+            }
+
+            // Calculate total cost of state and update unsat array - ONLY AFFECTED CLAUSES
             curTotalCost = 0;
-            unsat = new int[numSoft];
-            index = 0;
+            unsat.clear();
             sat = true;
             for (int c=0; c < numSoft; c++)
             {
                 // if current clause is unSAT, add to array
-                sat = checkSAT(c);
-                if (!sat)
+                sat = checkSATFloat(c, false);
+                if (!sat) 
                 {
-                    unsat[index] = c; 
-                    index++;
+                    unsat.add(c);
+                    curTotalCost += softCosts[c]; // Add to total cost if not SAT
                 }
-
-                // Add to total cost if not SAT
-                curTotalCost += (sat) ? 0 : softCosts[c]; 
             }
 
             // If current assignment is the best so far, save it
@@ -137,30 +160,118 @@ public class solver3 {
                 System.out.println("New Best Cost: " + Long.toString(bestCost));
             }
 
-            // Pick unsat soft clause with weighted probability:
-            curClause = pickClause(unsat, random);
-
-            // Get literals involved in selected clause:
-            start = softIndices[curClause];
-            end = softIndices[curClause+1];
-            makeScores = new long[end-start];
-            breakScores = new long[end-start];
-
-            // Calculate make and break scores for each literal in selected clause
-            for (int i = start; i < end; i++)
-            {
-                v = softLiterals[i];
-                vars.flip(v); // flip v for now and see what breaks
-                for (int c = ; c < ; c++)
-                {
-                    
-                }
-            }
-
-
             t++;
 
             if (t > T) {break;} // if time is up, end run
+
+            // Convert unsat ArrayList into int[]
+            unsat_arr = new int[unsat.size()];
+            for (int i = 0; i < unsat_arr.length; i++) {unsat_arr[i] = unsat.get(i);}
+            
+
+            // Small chance for random flip:
+            if (random.nextDouble() < RANDOM_CHANCE)
+            {
+                skip = false;
+                bestFlip = random.nextInt(numVars);
+                // Check hard clauses
+                for (int c=0; c < numClauses; c++) // check each clause per variable
+                {
+                    if (clauses[bestFlip*numClauses+c] == 1) // only consider clauses that contain the current variable
+                    {
+                        sign = (literals[c*numVars+bestFlip] < 0) ? -1 : 1; // check sign of literal
+
+                        // If a hard clause will get broken, ensure this variable isn't picked
+                        if (hardFloats[c]==0)
+                        {
+                            if (vars.get(bestFlip) && sign==1) {skip = true; break;}
+                            else if (!vars.get(bestFlip) && sign==-1) {skip = true; break;}
+                        }
+                    }
+                }
+
+                if (skip) {continue;}
+            
+            }
+            // Otherwise take greedy flip
+            else 
+            {
+                // Pick unsat soft clause with weighted probability:
+                curClause = pickClause(unsat_arr, random);
+
+                // Get literals involved in selected clause:
+                start = softIndices[curClause];
+                end = softIndices[curClause+1];
+                breakScores = new long[end-start];
+                makeScores = new long[end-start];
+
+                // Reset heuristic variables
+                bestScore = Long.MIN_VALUE;
+                bestFlip = -1;
+
+                // Calculate scores for each literal in selected clause
+                for (int i = start; i < end; i++)
+                {
+                    v = softLiterals[i];
+                    for (int c = ; c < ; c++)
+                    {
+                        
+                    }
+
+                    // Initialise make and break scores for v
+                    breakScores[v] = 0;
+                    makeScores[v] = 0;
+
+                    // Run for soft clauses
+                    for (int c=0; c < numClauses; c++) // check each clause per variable
+                    {
+                        if (clauses[v*numClauses+c] == 1) // only consider clauses that contain the current variable
+                        {
+                            sign = (literals[c*numVars+v] < 0) ? -1 : 1; // check sign of literal
+
+                            if (softFloats[c]==0) // check if break score will increase
+                            {
+                                if (vars.get(v) && sign==1) {breakScores[v] = breakScores[v] + softCosts[c];} // increase breakscore by cost of clause
+                                else if (!vars.get(v) && sign==-1) {breakScores[v] = breakScores[v] + softCosts[c];}
+                            }
+                            else if (softFloats[c]==-1) // check if make score will increase
+                            {
+                                if (!vars.get(v) && sign==1) {makeScores[v] = makeScores[v] + softCosts[c];} // increase makescore by cost of clause
+                                else if (vars.get(v) && sign==-1) {makeScores[v] = makeScores[v] + softCosts[c];}
+                            }
+                        }
+                    }
+                    
+                    // Check hard clauses
+                    for (int c=0; c < numClauses; c++) // check each clause per variable
+                    {
+                        if (clauses[v*numClauses+c] == 1) // only consider clauses that contain the current variable
+                        {
+                            sign = (literals[c*numVars+v] < 0) ? -1 : 1; // check sign of literal
+
+                            // If a hard clause will get broken, ensure this variable isn't picked by making score the min
+                            if (hardFloats[c]==0)
+                            {
+                                if (vars.get(v) && sign==1) {breakScores[v] = 0; makeScores[v]= Long.MIN_VALUE;}
+                                else if (!vars.get(v) && sign==-1) {breakScores[v] = 0; makeScores[v]= Long.MIN_VALUE;}
+                            }
+                        }
+                    }
+                    
+                    // Keep track of best score
+                    if (makeScores[v] - breakScores[v] > bestScore) // best is max
+                    {
+                        bestScore = makeScores[v] - breakScores[v];
+                        bestFlip = v; //heuristic
+                    }
+                }
+
+                // If all flips break a hard clause, skip to next unsat clause
+                if (bestFlip==-1) {continue;}
+            }
+
+            // Flip selected variable
+            vars.flip(bestFlip);
         }
 
         System.out.println("Final Best Cost: " + String.valueOf(bestCost));
@@ -194,44 +305,6 @@ public class solver3 {
             if (random.nextDouble() < costs[unif] / totalCost) {return unsat[unif];} // Accept with prob cost/total_cost
         }
     }
-
-    public int[] returnClause(boolean soft, int index){
-        int[] softLits=null, softInds=null;
-        int[] hardLits=null, hardInds=null;
-        // idk where u assign these, don't think you've pushed to git, but just replace these with ur actual ones.
-        
-        int[] outArr;
-        int size=0;
-
-        if (soft){
-            if (index < softInds.length-1)
-                size = softInds[index+1] - softInds[index];
-            else if (index == softInds.length -1) 
-                size = (softLits.length) - softInds[index];
-            else
-                throw new IndexOutOfBoundsException();
-             
-            outArr = new int[size];
-            for (int i = 0; i < size; i++){
-                outArr[i] = softLits[softInds[index]+i];
-            }
-            return outArr;
-        }
-        else{ // returning a hard clause
-            if (index < hardInds.length-1)
-                size = hardInds[index+1] - hardInds[index];
-            else if (index == hardInds.length -1) 
-                size = (hardLits.length) - hardInds[index];
-            else
-                throw new IndexOutOfBoundsException();
-             
-            outArr = new int[size];
-            for (int i = 0; i < size; i++){
-                outArr[i] = hardLits[hardInds[index]+i];
-            }
-            return outArr;
-        }
-    } 
 
     public static int checkFloat(int clauseToCheck, boolean hard)
     {
@@ -278,6 +351,14 @@ public class solver3 {
         
     }
 
+    // This is a quicker checkSAT method that relies on the float of the array
+    public static boolean checkSATFloat(int clauseToCheck, boolean hard)
+    {
+        if (hard) {return (hardFloats[clauseToCheck] >= 0);}
+        else {return (softFloats[clauseToCheck] >= 0);}
+    }
+
+    // First principles checkSAT
     public static boolean checkSAT(int clauseToCheck, boolean hard)
     {
         int sum = 0;
@@ -325,8 +406,7 @@ public class solver3 {
     public static int calcCurrentClauseCost(int clause, boolean hard)
     {   
         // If clause is satisfied, cost is 0, otherwise return associated cost
-        if (hard) {return (checkSAT(clause, true)) ? 0 : hardCost;}
-        else {return (checkSAT(clause, false)) ? 0 : softCosts[clause];}
-        
+        if (hard) {return (checkSATFloat(clause, true)) ? 0 : hardCost;}
+        else {return (checkSATFloat(clause, false)) ? 0 : softCosts[clause];}        
     }
 }
